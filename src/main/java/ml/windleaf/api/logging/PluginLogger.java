3ab26a425c1daf177cef8ddc351d8fa738f866bb
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-04 11:56:29 +0800

    :construction: add NameFormat for PluginLogger

--- src/main/java/ml/windleaf/api/logging/PluginLogger.java
+++ src/main/java/ml/windleaf/api/logging/PluginLogger.java
@@ -1,9 +1,18 @@
 package ml.windleaf.api.logging;
 
+import ml.windleaf.api.logging.format.NameFormat;
+
 public class PluginLogger {
-    private String name;
+    private final String name;
+    private final NameFormat nameFormat;
 
     public PluginLogger(String name) {
         this.name = name;
+        this.nameFormat = NameFormat.SQUARE_BRACKETS;
+    }
+
+    public PluginLogger(String name, NameFormat nameFormat) {
+        this.name = name;
+        this.nameFormat = nameFormat;
     }
 }
--- src/main/java/ml/windleaf/api/logging/format/NameFormat.java
+++ src/main/java/ml/windleaf/api/logging/format/NameFormat.java
@@ -1,8 +1,13 @@
 package ml.windleaf.api.logging.format;
 
+import org.bukkit.ChatColor;
+
 public enum NameFormat {
     EMPTY("{}"),
-    EMPTY_BOLD("");
+    EMPTY_BOLD(ChatColor.BOLD + "" + ChatColor.RESET),
+    SQUARE_BRACKETS("[{}]"),
+    SQUARE_BRACKETS_BOLD(ChatColor.BOLD + "[{}]" + ChatColor.RESET)
+    ;
 
     public final String content;
 
