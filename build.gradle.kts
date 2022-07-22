65aa9b6d7d8e99fdefe9e3f4ec3b8d76a483d5ec
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-22 18:37:27 +0800

    :heavy_plus_sign: add log4j dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,9 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-
     implementation("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
+    implementation("org.apache.logging.log4j:log4j-core:2.18.0")
 }
 
 tasks.getByName<Test>("test") {
