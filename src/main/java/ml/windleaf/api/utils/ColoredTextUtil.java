3d563c739a50577f27c539e7be5cfff37275ef9a
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-30 00:52:41 +0800

    :bug: fix '<>' in anonymous inner classes error

--- src/main/java/ml/windleaf/api/utils/ColoredTextUtil.java
+++ src/main/java/ml/windleaf/api/utils/ColoredTextUtil.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ColoredTextUtil {
-    private static final Map<String, String> mapping = new HashMap<>(){{
-        put("&", "§");
-    }};
+    private static final Map<String, String> mapping = new HashMap<>();
+
+    static {
+        mapping.put("&", "§");
+    }
 
     public static String color(String text) {
         for (String key : mapping.keySet()) {
