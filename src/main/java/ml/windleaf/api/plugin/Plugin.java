c2cbad5e09c769e3b914563319748c9ea03c35dc
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-30 00:17:20 +0800

    :sparkles: add Plugin abstract class

---
+++ src/main/java/ml/windleaf/api/plugin/Plugin.java
@@ -0,0 +1,43 @@
+package ml.windleaf.api.plugin;
+
+import ml.windleaf.api.logging.PluginLogger;
+import ml.windleaf.api.register.RegisterManager;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class Plugin extends JavaPlugin {
+    public static PluginLogger logger;
+
+    public abstract Plugin getInstance();
+
+    public String getPluginName() {
+        return this.getName();
+    }
+
+    public abstract String getPluginVersion();
+
+    public ChatColor getLoggerColor() {
+        return ChatColor.AQUA;
+    }
+
+    public abstract String getPackagePath();
+
+    public PluginLogger getPluginLogger() {
+        return logger;
+    }
+
+    @Override
+    public void onEnable() {
+        logger = new PluginLogger(this.getPluginName());
+        logger.setLoggerColor(this.getLoggerColor());
+        logger.logConsole("&aEnabling &b%s &e[v%s]&a...".formatted(this.getPluginName(), this.getPluginVersion()));
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        new RegisterManager(this.getInstance(), this.getPackagePath());
+    }
+
+    @Override
+    public void onDisable() {
+        logger.logConsole("&aDisabling &b%s&a...".formatted(this.getPluginName()));
+    }
+}
--- src/main/java/ml/windleaf/api/register/RegisterManager.java
+++ src/main/java/ml/windleaf/api/register/RegisterManager.java
@@ -20,23 +20,27 @@ public class RegisterManager {
     private static final PluginLogger logger = new PluginLogger("PlugApi");
     public static final Map<String, ICommand> commands = new HashMap<>();
 
-    public RegisterManager(JavaPlugin plugin, String packagePath) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        this.plugin = plugin;
-        ResolverUtil<ICommand> commandResolverUtil = new ResolverUtil<>();
-        ResolverUtil<IListener> listenerResolverUtil = new ResolverUtil<>();
-        List<Class<? extends ICommand>> commands = commandResolverUtil.getClassesBySuperclass(packagePath);
-        List<Class<? extends IListener>> listeners = listenerResolverUtil.getClassesBySuperclass(packagePath);
-        for (Class<? extends ICommand> iCommand : commands) {
-            ICommand iC = ((Class<ICommand>) iCommand).getDeclaredConstructor().newInstance();
-            logger.logConsole("Registering command => %s [%s]".formatted(iC.getClass().getSimpleName(), this.plugin.getName()));
-            RegisterManager.commands.put(iC.command(), iC);
+    public RegisterManager(JavaPlugin plugin, String packagePath) {
+        try {
+            this.plugin = plugin;
+            ResolverUtil<ICommand> commandResolverUtil = new ResolverUtil<>();
+            ResolverUtil<IListener> listenerResolverUtil = new ResolverUtil<>();
+            List<Class<? extends ICommand>> commands = commandResolverUtil.getClassesBySuperclass(packagePath);
+            List<Class<? extends IListener>> listeners = listenerResolverUtil.getClassesBySuperclass(packagePath);
+            for (Class<? extends ICommand> iCommand : commands) {
+                ICommand iC = ((Class<ICommand>) iCommand).getDeclaredConstructor().newInstance();
+                logger.logConsole("Registering command => %s [%s]".formatted(iC.getClass().getSimpleName(), this.plugin.getName()));
+                RegisterManager.commands.put(iC.command(), iC);
+            }
+            for (Class<? extends IListener> iListener : listeners) {
+                IListener iL = ((Class<IListener>) iListener).getDeclaredConstructor().newInstance();
+                logger.logConsole("Registering listener => %s [%s]".formatted(iL.getClass().getSimpleName(), this.plugin.getName()));
+                this.registerEvent(iL);
+            }
+            this.registerEvent(new CommandListener());
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
-        for (Class<? extends IListener> iListener : listeners) {
-            IListener iL = ((Class<IListener>) iListener).getDeclaredConstructor().newInstance();
-            logger.logConsole("Registering listener => %s [%s]".formatted(iL.getClass().getSimpleName(), this.plugin.getName()));
-            this.registerEvent(iL);
-        }
-        this.registerEvent(new CommandListener());
     }
 
     private void registerEvent(Listener listener) {
