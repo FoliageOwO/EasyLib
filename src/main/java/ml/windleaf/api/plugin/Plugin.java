1c4e45dfafa46e25afd88010b53dc2cf496b79f1
WindLeaf233 <mc.windleaf@foxmail.com>
2022-07-30 16:20:38 +0800

    :boom: BREAKING CHANGES: use 'superConfig' method to configure the Plugin

--- src/main/java/ml/windleaf/api/plugin/Plugin.java
+++ src/main/java/ml/windleaf/api/plugin/Plugin.java
@@ -6,40 +6,32 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class Plugin extends JavaPlugin {
+    protected String version;
+    protected String packagePath;
+    protected ChatColor loggerColor = ChatColor.GRAY;
     public static PluginLogger logger;
 
-    public abstract Plugin getInstance();
+    protected abstract void superConfig();
 
-    public String getPluginName() {
-        return this.getName();
-    }
-
-    public abstract String getPluginVersion();
-
-    public ChatColor getLoggerColor() {
-        return ChatColor.AQUA;
-    }
-
-    public abstract String getPackagePath();
-
-    public PluginLogger getPluginLogger() {
-        return logger;
+    @Override
+    public void onLoad() {
+        this.superConfig();
     }
 
     @Override
     public void onEnable() {
-        logger = new PluginLogger(this.getPluginName());
-        logger.setLoggerColor(this.getLoggerColor());
-        logger.logConsole(String.format("&aEnabling &b%s &e[v%s]&a...", this.getPluginName(), this.getPluginVersion()));
+        logger = new PluginLogger(this.getName());
+        logger.setLoggerColor(this.loggerColor);
+        logger.logConsole(String.format("&aEnabling &b%s &e[v%s]&a...", this.getName(), this.version));
         try {
             getConfig().options().copyDefaults();
             saveDefaultConfig();
         } catch (IllegalArgumentException ignore) {}
-        new RegisterManager(this.getInstance(), this.getPackagePath());
+        new RegisterManager(this, this.packagePath);
     }
 
     @Override
     public void onDisable() {
-        logger.logConsole(String.format("&aDisabling &b%s&a...", this.getPluginName()));
+        logger.logConsole(String.format("&aDisabling &b%s&a...", this.getName()));
     }
 }
