01283eac2b74af56ac595911814b2b2b5745ddda
WindLeaf233 <mc.windleaf@foxmail.com>
2022-09-08 22:13:50 +0800

    :art: improve color formatting method

--- src/main/java/ml/windleaf/api/utils/ColoredTextUtil.java
+++ src/main/java/ml/windleaf/api/utils/ColoredTextUtil.java
@@ -1,19 +1,9 @@
 package ml.windleaf.api.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
+import org.bukkit.ChatColor;
 
 public class ColoredTextUtil {
-    private static final Map<String, String> mapping = new HashMap<>();
-
-    static {
-        mapping.put("&", "§");
-    }
-
     public static String color(String text) {
-        AtomicReference<String> string = new AtomicReference<>(text);
-        mapping.keySet().forEach(key -> string.set(text.replaceAll(key, mapping.get(key))));
-        return string.get();
+        return ChatColor.translateAlternateColorCodes('&', text);
     }
 }
