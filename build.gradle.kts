88abd5b4e09bba5cf6d8c0c45ea5c7fdcbbbab8e
WindLeaf233 <mc.windleaf@foxmail.com>
2023-01-25 17:43:48 +0800

    :heavy_minus_sign: remove JUnit for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
     implementation("org.apache.logging.log4j:log4j-core:2.18.0")
     compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
 
 allprojects {
